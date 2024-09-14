8544ad7390f2f60ff03dce1e0580b5dd413199c3
Kylapurity <p.kihiu@alustudent.com>
2024-09-14 12:55:05 -0700

    update Matrices

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
-"""Define a function that will add the 2 matrices and result to either a interger and float"""
+""" defines function that adds two matrices """
+
+
+def matrix_shape(matrix):
+    """ returns list of integers representing dimensions of given matrix """
+
+    matrix_shape = []
+    while type(matrix) is list:
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
 
 
 def add_matrices(mat1, mat2):
-    """Add two matrices"""
-    if len(mat1) != len(mat2):
+    """ returns new matrix that is sum of two matrices added element-wise """
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
         return None
-    if isinstance(mat1[0], list):
-        return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
-    return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    if len(matrix_shape(mat1)) is 1:
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
+    return [add_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
