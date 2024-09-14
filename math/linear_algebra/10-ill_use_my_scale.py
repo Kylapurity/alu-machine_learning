d07e48e1614b4074aa5074ad881e50d817c373ed
Kylapurity <p.kihiu@alustudent.com>
2024-09-14 00:36:08 -0700

    update shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,15 +2,6 @@
 """Define Np shape"""
 
 
-import numpy as np
 def np_shape(matrix):
     """Function that calculates the shape of a numpy.ndarray"""
     return matrix.shape
-
-mat1 = np.array([1, 2, 3, 4, 5, 6])
-mat2 = np.array([])
-mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                 [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
-print(np_shape(mat1))
-print(np_shape(mat2))
-print(np_shape(mat3))
