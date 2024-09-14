7c4ad4713714e53e7591b4a0bbba4d77d98404a6
Kylapurity <p.kihiu@alustudent.com>
2024-09-14 12:29:36 -0700

    update

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,6 +4,6 @@
 import numpy as np
 
 
-def np_cat(mat1, mat2, axis=0)
+def np_cat(mat1, mat2, axis=0):
     """ returns new numpy.ndarray that is concatentation of two matrices """
     return np.concatenate((mat1, mat2), axis=axis)
