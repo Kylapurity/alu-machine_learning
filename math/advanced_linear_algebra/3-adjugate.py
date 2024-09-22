abe73eeb0fafad7b6c3cbc62d91f6caf90cc053e
Kylapurity <p.kihiu@alustudent.com>
2024-09-22 09:08:32 -0700

    update Adjugate

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -3,12 +3,11 @@
 This module contains the function to calculate the adjugate of a matrix.
 '''
 
-
 def adjugate(matrix):
     '''
     Returns the adjugate of a matrix.
     '''
-    if not matrix:
+    if not isinstance(matrix, list):
         raise TypeError("matrix must be a list of lists")
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -24,19 +23,18 @@ def adjugate(matrix):
     for i in range(len(matrix)):
         adjugate_matrix.append([])
         for j in range(len(matrix)):
-            sub_matrix = [row[:j] + row[j + 1:]
-   for row in (matrix[:i] + matrix[i + 1:])]
+            sub_matrix = [row[:j] + row[j + 1:] 
+                          for row in (matrix[:i] + matrix[i + 1:])]
             sign = (-1) ** (i + j)
             adjugate_matrix[i].append(sign * determinant(sub_matrix))
 
     return list(map(list, zip(*adjugate_matrix)))
 
-
 def determinant(matrix):
     '''
     Returns the determinant of a matrix.
     '''
-    if not matrix:
+    if not isinstance(matrix, list):
         raise TypeError("matrix must be a list of lists")
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -54,5 +52,6 @@ def determinant(matrix):
     det = 0
     for c in range(len(matrix)):
         det += ((-1) ** c) * matrix[0][c] * determinant([row[:c] + row[c + 1:] 
-   for row in matrix[1:]])
+                                                          for row in matrix[1:]])
     return det
+
