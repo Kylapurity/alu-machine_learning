f4c1f29234e6a42a90fb7c27be3250d83d7a16b7
Kylapurity <p.kihiu@alustudent.com>
2024-10-18 14:38:26 -0700

    update margical

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python3
+""""Function that calculates the marginal
+probability of obtaining the data"""
+
 import numpy as np
 
+
 def marginal(x, n, P, Pr):
-    """Function that calculates the marginal probability of obtaining the data."""
+    """Function that calculates the marginal probability of obtaining """
     # Validate inputs
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+          text = "x must be an integer that is greater than or equal to 0"
+        raise ValueError(text)
     if x > n:
         raise ValueError("x cannot be greater than n")
     if (not isinstance(P, np.ndarray)) or len(P.shape) != 1:
