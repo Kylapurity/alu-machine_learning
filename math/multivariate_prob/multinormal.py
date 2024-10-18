08308ed168f0f5a553bb2f93e05790ce54eaa3e2
Kylapurity <p.kihiu@alustudent.com>
2024-10-18 12:30:13 -0700

    update Multinormal

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,50 +1,53 @@
 #!/usr/bin/env python3
-'''A module that defines a MultiNormal class to represent a Multivariate Normal distribution'''
-
+"""
+Creates the MultiNormal class
+"""
 import numpy as np
 
 
 class MultiNormal:
-    '''A class that represents a Multivariate Normal distribution'''
+    """
+    Represents a Multivariate Normal distribution
+    """
 
     def __init__(self, data):
-        '''
-        Initialize the MultiNormal instance with data
-        Args:
-            data (numpy.ndarray): data set of shape (d, n)
-        '''
-        if not isinstance(data, np.ndarray) or len(data.shape) != 2:
+        """
+        class constructor
+        :param data: numpy.ndarray of shape (d, n) containing the data set:
+            n is the number of data points
+            d is the number of dimensions in each data point
+        """
+        if type(data) != np.ndarray:
             raise TypeError("data must be a 2D numpy.ndarray")
-        if data.shape[1] < 2:
+        if len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        d, n = data.shape
+        if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        self.mean = np.mean(data, axis=1, keepdims=True)
-        self.cov = self.calculate_covariance(data)
-        self.inv_cov = np.linalg.inv(self.cov)
-        self.det_cov = np.linalg.det(self.cov)
-        self.dim = data.shape[0]  # d is the number of dimensions
-        self.norm_const = 1 / (np.sqrt((2 * np.pi) ** self.dim * self.det_cov))
-
-    def calculate_covariance(self, data):
-        '''Calculates the covariance matrix manually without using numpy.cov'''
-        mean = np.mean(data, axis=1, keepdims=True)
-        data_centered = data - mean
-        cov_matrix = np.dot(data_centered, data_centered.T) / (data.shape[1] - 1)
-        return cov_matrix
+        self.mean = data.mean(axis=1).reshape(d, 1)
+        X_mean = data - self.mean
+        self.cov = (X_mean @ X_mean.T) / (n - 1)
 
     def pdf(self, x):
-        '''
-        Calculate the probability density function (PDF) for a given point
-        Args:
-            x (numpy.ndarray): data point of shape (d, 1)
-        Returns:
-            float: the value of the PDF at the given point
-        '''
-        if not isinstance(x, np.ndarray):
+        """
+        calculates the PDF
+        :param x: numpy.ndarray of shape (d, 1) containing the data point whose
+        PDF should be calculated
+            d is the number of dimensions of the Multinomial instance
+        :return: the value of the PDF
+        """
+        if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
-        if x.shape != (self.dim, 1):
-            raise ValueError(f"x must have the shape ({self.dim}, 1)")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
 
-        diff = x - self.mean
-        exponent = -0.5 * np.dot(diff.T, np.dot(self.inv_cov, diff))
-        return float(self.norm_const * np.exp(exponent))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        first = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
+        second = np.dot((x - self.mean).T, inv)
+        third = np.dot(second, (x - self.mean) / -2)
+        pdf = first * np.exp(third)
+
+        return pdf[0][0]
