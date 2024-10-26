b515a5bbaae49435b6545d04054e43214a08ffda
Kylapurity <p.kihiu@alustudent.com>
2024-10-26 04:39:14 -0700

    update padding

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -15,7 +15,9 @@ def convolve_grayscale_padding(images, kernel, padding):
     ph, pw = padding
 
     # Pad the images with zeros
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    padded_images = np.pad(
+        images, ((0, 0), (ph, ph), (pw, pw)), mode='constant'
+    )
 
     # Calculate the dimensions of the output
     output_h = h + 2 * ph - kh + 1
@@ -28,7 +30,7 @@ def convolve_grayscale_padding(images, kernel, padding):
     for i in range(output_h):
         for j in range(output_w):
             # Extract the region of interest
-            region = padded_images[:, i:i+kh, j:j+kw]
+            region = padded_images[:, i:i + kh, j:j + kw]
             # Perform element-wise multiplication and sum the result
             output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
 
